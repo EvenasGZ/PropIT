4c53d237a60caecdc57ccbdc86660f25c10c2bfe
Rudolf <119478278+EvenasGZ@users.noreply.github.com>
2025-03-19 20:26:01 +0800

    Exp Block.py

--- Exp Block.py
+++ Exp Block.py
@@ -1,1 +1,57 @@
+import hashlib
+import time
 
+class Block:
+    def __init__(self, index, previous_hash, timestamp, data, hash):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.timestamp = timestamp
+        self.data = data
+        self.hash = hash
+
+def calculate_hash(index, previous_hash, timestamp, data):
+    value = str(index) + str(previous_hash) + str(timestamp) + str(data)
+    return hashlib.sha256(value.encode('utf-8')).hexdigest()
+
+def create_genesis_block():
+    # Manually construct a block with index 0 and arbitrary previous hash
+    return Block(0, "0", time.time(), "Genesis Block", calculate_hash(0, "0", time.time(), "Genesis Block"))
+
+def create_new_block(previous_block, data):
+    index = previous_block.index + 1
+    timestamp = time.time()
+    hash = calculate_hash(index, previous_block.hash, timestamp, data)
+    return Block(index, previous_block.hash, timestamp, data, hash)
+
+# Create the blockchain and add the genesis block
+blockchain = [create_genesis_block()]
+previous_block = blockchain[0]
+
+# Add blocks to the blockchain
+num_of_blocks_to_add = 5
+
+for i in range(0, num_of_blocks_to_add):
+    data = f"Block {i} Data"
+    new_block = create_new_block(previous_block, data)
+    blockchain.append(new_block)
+    previous_block = new_block
+    print(f"Block #{new_block.index} has been added to the blockchain!")
+    print(f"Hash: {new_block.hash}\n")
+
+# Verify the integrity of the blockchain
+def is_chain_valid(chain):
+    for i in range(1, len(chain)):
+        current_block = chain[i]
+        previous_block = chain[i - 1]
+
+        if current_block.hash != calculate_hash(current_block.index, previous_block.hash, current_block.timestamp, current_block.data):
+            print("Current block hash is invalid!")
+            return False
+
+        if current_block.previous_hash != previous_block.hash:
+            print("Previous block hash is invalid!")
+            return False
+
+    return True
+
+print("Blockchain valid:", is_chain_valid(blockchain)
